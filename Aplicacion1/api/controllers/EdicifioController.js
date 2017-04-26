086f20a8726774f07ac5051d460724b9703c5821
JonathaPachacama <USRDEL@USRDEL12.local>
2017-04-26 14:20:05 -0500

    commit node_modules

---
+++ Aplicacion1/api/controllers/EdicifioController.js
@@ -0,0 +1,11 @@
+/**
+ * EdicifioController
+ *
+ * @description :: Server-side logic for managing Edicifios
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
