e3c17145c0632dabc7c8036bb89453ac9f7efe12
JonathaPachacama <USRDEL@USRDEL12.local>
2017-05-02 16:59:53 -0500

    conectar a mongo mysql postgret

--- Aplicacion1/api/models/Casa.js
+++ Aplicacion1/api/models/Casa.js
@@ -9,6 +9,7 @@ module.exports = {
 
   attributes: {
 
+    connection: 'conexionMysqlDocker',
     nombre:{
       type : 'String'
 
--- Aplicacion1/api/models/Edicifio.js
+++ Aplicacion1/api/models/Edicifio.js
@@ -9,6 +9,8 @@ module.exports = {
 
   attributes: {
 
+    connection: 'conexionPostgresqlDocker',
+
     nombre:{
       type : 'String'
 
