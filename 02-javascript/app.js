cd14e8e36be14a045f5cdbf153497e68e1ffa0a7
JonathaPachacama <USRDEL@USRDEL12.local>
2017-05-09 15:53:43 -0500

    cambio objeto app.js

--- 02-javascript/app.js
+++ 02-javascript/app.js
@@ -6,3 +6,29 @@
 //comentarios una sola linea
 
 console.log('hello Kitty')
+
+
+//crear un objeto con mi nombre (nombre, apellido y una funcion que imprima el nombre y el apellido)
+
+
+var jonathan = {
+
+    nombre:"Jonathan",
+    apellido:"Pachacama",
+    //fechaNacimiento:new Date(),
+    imprimir:function () {
+
+        //console.log(adrian.nombre,adrian.apellido) una forma
+        console.log(this.nombre,this.apellido) //this me dice que me coja del objeto, poner nombres no es buena idea
+
+    },
+    //ano:function () {
+      //  return this.fechaNacimiento.getFullYear()
+    //}
+
+}
+
+
+jonathan.imprimir()
+
+
