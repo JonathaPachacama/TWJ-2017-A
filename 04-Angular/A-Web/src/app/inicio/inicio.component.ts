22bcbf57927562822d420543535be74d6764c7d4
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-05-30 16:59:47 -0500

    ngfor
    revisar commit julia

--- 04-Angular/A-Web/src/app/inicio/inicio.component.ts
+++ 04-Angular/A-Web/src/app/inicio/inicio.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  nombre: string = "Jonathan";
+  //command + a(para seleccionas) y luedo comman command+alt+l
+  arregloUsuarios = [
+    {
+      nombre:"Jonathan",
+      apellido:"pachacama",
+      conectado:true
+    },
+    {
+      nombre:"Mashi",
+      apellido:"correa",
+      conectado:true
+    },
+    {
+      nombre: "Abdala",
+      apellido: "Bucaran",
+      conectado:false
+    }];
 
-  ngOnInit() {
+
+  constructor() {
   }
 
-}
+  ngOnInit() {
+
+  }
+
+    cambiarNombre():void{
+      console.log("Entro");
+    this.nombre = "Rafico a lenin";
+    }
+    cambiarOtroNombre()
+    {
+      this.nombre = "Lenin a Rafico";
+    }
+    cambiarNombreInput(nombreEtiqueta)
+    {
+      console.log(nombreEtiqueta.value);
+      console.log(nombreEtiqueta.type);
+      console.log(nombreEtiqueta.placeholder);
+      this.nombre=nombreEtiqueta.value;
+
+    }
+  }
+
+
+
