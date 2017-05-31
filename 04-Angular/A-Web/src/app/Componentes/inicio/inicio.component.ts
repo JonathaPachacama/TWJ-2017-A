246ca0c36cf5606006c8bdff6083f8e13843a181
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-05-31 08:43:48 -0500

    etiquetas de eventos databinding
    tambien se cambion la carpeta de Inicio

---
+++ 04-Angular/A-Web/src/app/Componentes/inicio/inicio.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+
+  nombre: string = "Jonathan";
+  //command + a(para seleccionas) y luedo comman command+alt+l
+  // arregloUsuarios = [
+  //   {
+  //     nombre:"Jonathan",
+  //     apellido:"pachacama",
+  //     conectado:true
+  //   },
+  //   {
+  //     nombre:"Mashi",
+  //     apellido:"correa",
+  //     conectado:true
+  //   },
+  //   {
+  //     nombre: "Abdala",
+  //     apellido: "Bucaran",
+  //     conectado:false
+  //   }];
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+    cambiarNombre():void{
+      console.log("Entro");
+    this.nombre = "Rafico a lenin";
+    }
+    cambiarOtroNombre()
+    {
+      this.nombre = "Lenin a Rafico";
+    }
+    // cambiarNombreInput(nombreEtiqueta)
+    // {
+    //   console.log(nombreEtiqueta.value);
+    //   console.log(nombreEtiqueta.type);
+    //   console.log(nombreEtiqueta.placeholder);
+    //   this.nombre=nombreEtiqueta.value;
+    //
+    // }
+  }
+
+
+
